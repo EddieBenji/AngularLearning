ffb61372938602b1972e7bbf2a51b318851561ae
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-15 12:43:14 -0600

    connected db with the front end (front end hasn't been touched).

--- routes/app.js
+++ routes/app.js
@@ -3,15 +3,22 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('node');
+    User.findOne({}, function (error, doc) {
+        //will be called after the task is completed
+        if (error) {
+            return res.send('Error!');
+        }
+        //I need to return the view, once the database responds:
+        res.render('node',{email: doc.email});
+    });
 });
 
 router.post('/', function (req, res, next) {
     var email = req.body.email;
     var user = new User({
-        firstName:'Lalo',
-        lastName:'Vazquez',
-        password:'super-secret',
+        firstName: 'Lalo',
+        lastName: 'Vazquez',
+        password: 'super-secret',
         email: email
     });
     user.save();
