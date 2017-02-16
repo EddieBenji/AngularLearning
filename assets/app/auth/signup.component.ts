27c5edfc6d705cc85e996d6a7a5a39e6a7c95ef4
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-16 16:31:50 -0600

    SignUp form finished.

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,9 +1,28 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 @Component({
-    selector:'app-signup',
-    templateUrl:'./signup.component.html'
+    selector: 'app-signup',
+    templateUrl: './signup.component.html'
 })
 
-export class SignupComponent{
+export class SignupComponent implements OnInit {
+    signUpForm: FormGroup;
 
+    ngOnInit() {
+        const EMAIL_REGEXP = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+        this.signUpForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern(EMAIL_REGEXP)
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
+
+    onSubmit() {
+        console.log(this.signUpForm);
+        this.signUpForm.reset();
+    }
 }
