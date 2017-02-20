138fa33e26f992de6857de9f68fdc5c431de7bc9
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-20 15:58:24 -0600

    now, every message is added in mongo (back and front end linked).

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,9 +1,27 @@
+import {Http, Response, Headers} from "@angular/http";
+import {Injectable} from "@angular/core";
+import 'rxjs/Rx';
+import {Observable} from "rxjs";
+
 import {Message} from "./message.model";
+
+//with this we are able to add the http service.
+@Injectable()
 export class MessageService {
     private messages: Message[] = [];
 
+    constructor(private http: Http) {
+    }
+
     addMessage(message: Message) {
         this.messages.push(message);
+        const body = JSON.stringify(message);
+        const headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        return this.http.post('http://localhost:3000/message', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 
     getMessages() {
