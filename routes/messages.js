4cc7d2854689fd7325a9e4f12f80e2e1c63c7d20
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-20 16:36:08 -0600

    update message endpoint finished

--- routes/messages.js
+++ routes/messages.js
@@ -5,7 +5,7 @@ var Message = require('../models/message');
 
 router.get('/', function (req, res, next) {
     Message.find()
-        .exec(function(err, messages){
+        .exec(function (err, messages) {
             if (err) {
                 return res.status(500).json({
                     title: 'An error occurred',
@@ -14,7 +14,7 @@ router.get('/', function (req, res, next) {
             }
 
             res.status(200).json({
-                message:'Success',
+                message: 'Success',
                 obj: messages
             });
         });
@@ -38,6 +38,38 @@ router.post('/', function (req, res, next) {
             obj: result
         });
 
+    });
+});
+
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+
+        if (!message) {
+            return res.status(500).json({
+                title: 'No Message Found!',
+                error: {message: 'Message not found'}
+            });
+        }
+
+        message.content = req.body.content;
+        message.save(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(201).json({
+                message: 'Updated message!',
+                obj: result
+            });
+        });
     });
 });
 
