efaaf541edef4529af6880593b07ff6869fe3564
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-21 14:51:10 -0600

    Messages and users connected in the backend

--- models/message.js
+++ models/message.js
@@ -4,11 +4,19 @@ var mongoose = require('mongoose');
 
 //This is the general schema:
 var Schema = mongoose.Schema;
+var User = require('./user');
 
 //This is the schema I'm going to create (something like the blueprint of the database you're about to create):
 var schema = new Schema({
     content: {type: String, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+schema.post('remove', function (message) {
+    User.findById(message.user, function (err, user) {
+        user.messages.pull(message);
+        user.save();
+    });
 });
 
 //now, the models:
--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
+var User = require('../models/user');
 var Message = require('../models/message');
 
 router.get('/', function (req, res, next) {
@@ -20,25 +22,51 @@ router.get('/', function (req, res, next) {
         });
 });
 
-router.post('/', function (req, res, next) {
-    // res.render('index');
-    var message = new Message({
-        content: req.body.content
+// ALL ENDPOINTS WILL EXECUTE THIS FUNCTION. FROM THIS POINT FORWARD.
+router.use('/', function (req, res, next) {
+    jwt.verify(req.query.token, 'secret', function (err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                title: 'Not Authenticated',
+                error: err
+            });
+        }
+        next();
     });
-    message.save(function (err, result) {
+});
+
+router.post('/', function (req, res, next) {
+    var decoded = jwt.decode(req.query.token);
+    User.findById(decoded.user._id, function (err, user) {
         if (err) {
             return res.status(500).json({
                 title: 'An error occurred',
                 error: err
             });
         }
-        //You can write return or not, 'cuz there are not code to execute after this statement.
-        res.status(201).json({
-            message: 'Saved message!',
-            obj: result
-        });
 
+        var message = new Message({
+            content: req.body.content,
+            user: user
+        });
+        message.save(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            user.messages.push(result);
+            user.save();
+            //You can write return or not, 'cuz there are not code to execute after this statement.
+            res.status(201).json({
+                message: 'Saved message!',
+                obj: result
+            });
+
+        });
     });
+
 });
 
 router.patch('/:id', function (req, res, next) {
