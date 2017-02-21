d8c7ef1ad0b833ab5d099cf4a22a032f8c5835eb
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-21 16:52:42 -0600

    added auth module, so now lazy loading is happening. Only when you click in authentication tab, the routes (and page) will be loaded

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,14 +1,10 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
-import {ReactiveFormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {AuthenticationComponent} from "./auth/authentication.component";
 import {HeaderComponent} from "./header.component";
 import {routing} from "./app.routing";
-import {LogoutComponent} from "./auth/logout.component";
-import {SignupComponent} from "./auth/signup.component";
-import {SigninComponent} from "./auth/signin.component";
 import {AuthService} from "./auth/auth.service";
 import {ErrorComponent} from "./errors/error.component";
 import {ErrorService} from "./errors/error.service";
@@ -19,15 +15,11 @@ import {MessageModule} from "./messages/message.module";
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SignupComponent,
-        SigninComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule,
         routing,
-        ReactiveFormsModule,
         HttpModule,
         MessageModule
     ],
---
+++ assets/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import {LogoutComponent} from "./logout.component";
+import {SignupComponent} from "./signup.component";
+import {SigninComponent} from "./signin.component";
+
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {ReactiveFormsModule} from "@angular/forms";
+import {authRouting} from "./auth.routing";
+
+@NgModule({
+    declarations: [
+        LogoutComponent,
+        SignupComponent,
+        SigninComponent,
+    ],
+    imports:[
+        CommonModule,
+        ReactiveFormsModule,
+        authRouting
+    ]
+})
+export class AuthModule {
+
+}
