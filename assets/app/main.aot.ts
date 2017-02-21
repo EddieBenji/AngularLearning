5b9702f049a55194e218a4a3582150d95a8bce32
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-21 17:46:20 -0600

    project completed.

--- assets/app/main.aot.ts
+++ assets/app/main.aot.ts
@@ -1,8 +1,8 @@
 import './polyfills';
 import { platformBrowser } from "@angular/platform-browser";
-import { enableProdMode } from "@angular/core";
-
 import { AppModuleNgFactory } from './app.module.ngfactory';
+
+import { enableProdMode } from "@angular/core";
 
 enableProdMode();
 
