4f9466bcf967efb9148fb2882cb6f482a4340f39
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-21 15:08:09 -0600

    code refactored

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -14,13 +14,22 @@ export class MessageService {
     constructor(private http: Http) {
     }
 
+    getBaseUrl() {
+        return 'http://localhost:3000/message';
+    }
+
+    getToken() {
+        const token = localStorage.getItem('token');
+        return token ? '?token=' + token : '';
+    }
+
     addMessage(message: Message) {
         // this.messages.push(message);
         const body = JSON.stringify(message);
         const headers = new Headers({
             'Content-Type': 'application/json'
         });
-        return this.http.post('http://localhost:3000/message', body, {headers: headers})
+        return this.http.post(this.getBaseUrl() + this.getToken(), body, {headers: headers})
             .map((response: Response) => {
                 const result = response.json();
                 const message = new Message(result.obj.content, 'Dummy', result.obj._id, null);
@@ -32,7 +41,7 @@ export class MessageService {
 
     getMessages() {
         // return this.messages;
-        return this.http.get('http://localhost:3000/message')
+        return this.http.get(this.getBaseUrl())
             .map((response: Response) => {
                 const messages = response.json().obj;
                 //Let's transform the mssage from the back to the front
@@ -55,14 +64,14 @@ export class MessageService {
         const headers = new Headers({
             'Content-Type': 'application/json'
         });
-        return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
+        return this.http.patch(this.getBaseUrl() + message.messageId + this.getToken(), body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
     }
 
     deleteMessage(message: Message) {
         this.messages.splice(this.messages.indexOf(message), 1);
-        return this.http.delete('http://localhost:3000/message/' + message.messageId)
+        return this.http.delete(this.getBaseUrl() + '/' + message.messageId + this.getToken())
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
     }
