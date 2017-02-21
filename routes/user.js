ed8951a3a84ca0f46a86032629cfd0dc56982161
Eduardo Canche <eduardo_benji@hotmail.com>
2017-02-21 12:17:34 -0600

    sign in method completed

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
 var User = require('../models/user');
+var jwt = require('jsonwebtoken');
 
 router.post('/', function (req, res, next) {
     // res.render('index');
@@ -23,6 +24,41 @@ router.post('/', function (req, res, next) {
             obj: result
         });
     });
+});
+
+router.post('/signin', function (err, res, next) {
+
+    User.findOne({email: req.body.email}, function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if (!user) {
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            });
+        }
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            //is not valid
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            });
+        }
+
+        //User is valid :)
+        //Let's create the token (it's measure in seconds). 60*60*2 -> seconds * minutes * hours (only hours should be changed)
+        var token = jwt.sign({user: user}, 'secret', {expiresIn: 60 * 60 * 2});
+        res.status(200).json({
+            message: 'Successfully logged id',
+            token: token,
+            userId: user._id
+        });
+    });
+
 });
 
 module.exports = router;
